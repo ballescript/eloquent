72c251330a23ce41b86a796c81e867de7d67f1c8
elballes <33511941+ballescript@users.noreply.github.com>
2020-09-30 12:37:54 -0700

    Create The_sum_of_a_range.js

---
+++ Data_Structures/The_sum_of_a_range.js
@@ -0,0 +1,15 @@
+let range = (start, end, step=1) => {
+  let numbers = [];
+  for(let i = start; i <= end; i += step) {
+    numbers.push(i)
+  }
+  return numbers;
+}
+
+let sum = numbers => {
+  let result = 0;
+  for(number of numbers) {
+    result += number
+  }
+  return result;
+}
