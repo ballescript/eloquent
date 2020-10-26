670a413d80f5b52429608749d4a2034cc1ae7cfc
elballes <33511941+ballescript@users.noreply.github.com>
2020-10-26 13:26:15 -0600

    Create Everything.js

---
+++ 5_Higher-Order_Functions/Everything.js
@@ -0,0 +1,12 @@
+function every(array, test) {
+  for(let i = 0; i < array.length; i++) {
+    if(!test(array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function everySome(array, test) {
+  return !array.some(x => !test(x))
+}
